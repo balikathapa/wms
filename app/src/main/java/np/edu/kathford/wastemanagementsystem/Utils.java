14019c9d0c59f8d0bf4a99fd46555e1f23d08c03
Balika Thapa <thapabalika6@gmail.com>
2023-04-30 21:26:30 +0545

    Login and Signup

---
+++ app/src/main/java/np/edu/kathford/wastemanagementsystem/Utils.java
@@ -0,0 +1,11 @@
+package np.edu.kathford.wastemanagementsystem;
+
+public class Utils {
+    //Email Validation pattern
+    public static final String regEx = "";
+
+    //Fragments Tags
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String SignUp_Fragment = "SignUp_Fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+}
